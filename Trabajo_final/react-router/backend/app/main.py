f7e554b411d05446201b7087c567a5feacdebdda
Enriquegarciayranzo <Enriquegarciayranzo@gmail.com>
2026-01-18 23:02:16 +0100

    update

--- Trabajo_final/react-router/backend/app/main.py
+++ Trabajo_final/react-router/backend/app/main.py
@@ -22,6 +22,10 @@ app = FastAPI(lifespan=lifespan)
 # Configure CORS policy.
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
     allow_origin_regex=r"^https://.*\.vercel\.app$",
     allow_credentials=True,
     allow_methods=["*"],
