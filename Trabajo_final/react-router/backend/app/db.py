a4bea0edf500764da910f5b25d4b43f8ff221d25
Enriquegarciayranzo <Enriquegarciayranzo@gmail.com>
2026-01-19 03:00:34 +0100

    update

--- Trabajo_final/react-router/backend/app/db.py
+++ Trabajo_final/react-router/backend/app/db.py
@@ -5,18 +5,15 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL is not set")
 
-# Render suele dar URL con 'postgresql://'. SQLAlchemy acepta 'postgresql://'
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
-
+# Fuerza psycopg (v3)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True, module=None)
 
 def create_db_and_tables():
     from app.models.product import Product
     from app.models.order import Order, OrderItem
     from app.models.user import User
     from app.models.cart import CartItem
-
     SQLModel.metadata.create_all(engine)
-
 
 def get_session():
     with Session(engine) as session:
