9cf1e6e76d0246a2b72ae6fd2a29274e2442f7ff
Enriquegarciayranzo <Enriquegarciayranzo@gmail.com>
2026-01-19 02:30:01 +0100

    update

--- Trabajo_final/react-router/backend/app/db.py
+++ Trabajo_final/react-router/backend/app/db.py
@@ -1,8 +1,11 @@
 from pathlib import Path
 from sqlmodel import Session, SQLModel, create_engine
 
-BASE_DIR = Path(__file__).resolve().parent  # carpeta app/
-sqlite_file_path = BASE_DIR / "database.db"
+# Render Disk montado en /var/data
+DB_DIR = Path("/var/data")
+DB_DIR.mkdir(parents=True, exist_ok=True)
+
+sqlite_file_path = DB_DIR / "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_path}"
 
 connect_args = {"check_same_thread": False}
@@ -10,10 +13,12 @@ engine = create_engine(sqlite_url, connect_args=connect_args)
 
 
 def create_db_and_tables():
+    # Registrar modelos
     from app.models.product import Product
     from app.models.order import Order, OrderItem
     from app.models.user import User
     from app.models.cart import CartItem
+
     SQLModel.metadata.create_all(engine)
 
 
